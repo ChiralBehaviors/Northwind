68fc19599013e64a8f0b5958f5b00daee7059674
Hellblazer <hellblazer@me.com>
2015-05-15 21:29:09 -0700

    rename order item to item detail.  use facets in worspace dsl.

--- northwind/src/main/java/com/chiralbehaviors/northwind/products/ItemDetail.java
+++ northwind/src/main/java/com/chiralbehaviors/northwind/products/ItemDetail.java
@@ -20,8 +20,12 @@
 
 package com.chiralbehaviors.northwind.products;
 
+import java.math.BigDecimal;
+
 import com.chiralbehaviors.CoRE.phantasm.Phantasm;
 import com.chiralbehaviors.CoRE.product.Product;
+import com.chiralbehaviors.annotations.Edge;
+import com.chiralbehaviors.annotations.Key;
 import com.chiralbehaviors.annotations.State;
 
 /**
@@ -29,6 +33,23 @@ import com.chiralbehaviors.annotations.State;
  *
  */
 @State(workspace = "uri:http://ultrastructure.me/ontology/com.chiralbehaviors/demo/northwind/v1")
-public interface OrderItem extends Phantasm<Product> {
+public interface ItemDetail extends Phantasm<Product> {
+    BigDecimal getDiscountPercentage();
 
+    Integer getQuantity();
+
+    BigDecimal getTaxRate();
+
+    BigDecimal getUnitPrice();
+
+    void setDiscountPercentage(BigDecimal percentage);
+
+    void setQuantity(Integer quantity);
+
+    void setTaxRate(BigDecimal rate);
+
+    void setUnitPrice(BigDecimal price);
+
+    @Edge(@Key(name = ""))
+    PricedProduct getProduct();
 }
