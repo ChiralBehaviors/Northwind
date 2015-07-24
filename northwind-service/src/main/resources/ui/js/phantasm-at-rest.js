048bc92b552c83c816a05c0bca26d298b3e9176e
Hellblazer <hellblazer@me.com>
2015-07-24 15:19:49 -0700

    non anon function

--- northwind-service/src/main/resources/ui/js/phantasm-at-rest.js
+++ northwind-service/src/main/resources/ui/js/phantasm-at-rest.js
@@ -1,35 +1,30 @@
-(function() {
+var phantasm = angular.module('phantasm-at-rest', [ 'restangular' ]);
 
-	var phantasm = angular.module('phantasm-at-rest', [ 'restangular' ]);
+phantasm.config([ 'RestangularProvider', function(RestangularProvider) {
+	RestangularProvider.setBaseUrl("/json-ld");
+} ]);
 
-	phantasm.config([ 'RestangularProvider', function(RestangularProvider) {
-		RestangularProvider.setBaseUrl("/json-ld");
-	} ]);
+phantasm.factory("Ruleform", [ "Restangular", function(Restangular) {
+	var service = Restangular.service("ruleform");
+	return service;
+} ]);
 
-	phantasm.factory("Ruleform", [ "Restangular", function(Restangular) {
-		var service = Restangular.service("ruleform");
-		return service;
-	} ]);
+phantasm.factory("Facet", [ "Restangular", function(Restangular) {
+	var service = Restangular.service("facet");
+	return service;
+} ]);
 
-	phantasm.factory("Facet", [ "Restangular", function(Restangular) {
-		var service = Restangular.service("facet");
-		return service;
-	} ]);
+phantasm.factory("Workspace", [ "Restangular", function(Restangular) {
+	var service = Restangular.service("workspace");
+	return service;
+} ]);
 
-	phantasm.factory("Workspace", [ "Restangular", function(Restangular) {
-		var service = Restangular.service("workspace");
-		return service;
-	} ]);
+phantasm.factory("WorkspaceMediated", [ "Restangular", function(Restangular) {
+	var service = Restangular.service("workspace-mediated");
+	return service;
+} ]);
 
-	phantasm.factory("WorkspaceMediated", [ "Restangular",
-			function(Restangular) {
-				var service = Restangular.service("workspace-mediated");
-				return service;
-			} ]);
-
-	phantasm.factory("WorkspaceLookup", [ "Restangular", function(Restangular) {
-		var service = Restangular.service("workspace/lookup");
-		return service;
-	} ]);
-
-})();
+phantasm.factory("WorkspaceLookup", [ "Restangular", function(Restangular) {
+	var service = Restangular.service("workspace/lookup");
+	return service;
+} ]);
